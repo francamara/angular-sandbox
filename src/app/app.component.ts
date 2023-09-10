a923cc240573c12fc3bbadad35b96a56fb67084e
francamaradev <francamaradev@gmail.com>
2023-09-10 00:46:11 -0700

    feat: routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular-sandbox';
+
+  readonly routerLinks: {link: string, displayName: string}[] = [
+    {
+      link: 'fizz-buzz',
+      displayName: 'Fizz Buzz'
+    },
+    {
+      link: 'todo-list',
+      displayName: 'TODO List'
+    },
+    {
+      link: 'http-req',
+      displayName: 'HTTP Req'
+    },
+  ]
 }
