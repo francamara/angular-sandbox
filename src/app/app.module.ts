e697390e332d3b5b2037b8cc638caf5e02154aac
francamaradev <francamaradev@gmail.com>
2023-09-10 00:47:14 -0700

    more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { FizzBuzzComponent } from './fizz-buzz/fizz-buzz.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatChipsModule} from '@angular/material/chips';
+import { MatChipsModule } from '@angular/material/chips';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { HttpReqComponent } from './http-req/http-req.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     FizzBuzzComponent,
-    TodoListComponent
+    TodoListComponent,
+    HttpReqComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +33,15 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     FormsModule,
     MatTableModule,
     MatChipsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
     RouterModule.forRoot([
       {path: 'fizz-buzz', component: FizzBuzzComponent},
-      {path: 'todo-list', component: TodoListComponent}
+      {path: 'todo-list', component: TodoListComponent},
+      {path: 'http-req', component: HttpReqComponent}
     ])
   ],
   providers: [],
